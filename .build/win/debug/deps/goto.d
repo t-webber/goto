e4bf43cfb4b7dfd225ce1c490584dcbce8154ecb
Tom Webber <t.c.w.webber@gmail.com>
2024-04-10 09:49:21 +0200

    cleaning gitignore

--- .build/win/debug/deps/goto.d
+++
@@ -1,9 +0,0 @@
-D:\files\dev\rust\goto\.build\win\debug\deps\goto.exe: src\main.rs src\commands.rs src\dirs.rs src\errors.rs src\hist.rs
-
-D:\files\dev\rust\goto\.build\win\debug\deps\goto.d: src\main.rs src\commands.rs src\dirs.rs src\errors.rs src\hist.rs
-
-src\main.rs:
-src\commands.rs:
-src\dirs.rs:
-src\errors.rs:
-src\hist.rs:
